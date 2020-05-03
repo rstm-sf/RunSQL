1d960faba9eba7e6a1678361a243324f1e095ad8
Rustam Sayfutdinov <rstm.sf@gmail.com>
2020-05-03 18:12:17 +0300

    Fix CurrentDirectory when start app

--- src/RunSQL/Program.cs
+++ src/RunSQL/Program.cs
@@ -1,12 +1,19 @@
-﻿using Avalonia;
+﻿using System.IO;
+using System.Reflection;
+using Avalonia;
 using Avalonia.Logging.Serilog;
 
 namespace RunSQL
 {
     class Program
     {
-        public static void Main(string[] args) =>
+        public static void Main(string[] args)
+        {
+            // https://github.com/dotnet/project-system/issues/2239
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+
             BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
 
         public static AppBuilder BuildAvaloniaApp() =>
             AppBuilder.Configure<App>()
