18f6a10e5e74443a478dd10422707baa74077b45
Rustam Sayfutdinov <rstm.sf@gmail.com>
2020-05-06 12:13:09 +0300

    Add test project

---
+++ src/RunSQL.Tests/ViewModels/MainWindowViewModelTests.cs
@@ -0,0 +1,9 @@
+﻿using RunSQL.ViewModels;
+
+namespace RunSQL.Tests.ViewModels
+{
+    public class MainWindowViewModelTests
+    {
+        private readonly MainWindowViewModel _viewModel;
+    }
+}
